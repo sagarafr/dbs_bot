fd640fb4fdf9067c14abfd6cd0fb774917ed9e03
sagarafr <sagarafr@gmail.com>
2017-05-28 10:22:49 +0200

    chore(cmd): Change start message

--- commands/cmd_start.py
+++ commands/cmd_start.py
@@ -4,4 +4,4 @@ from commands import Command
 @Command()
 def start(bot, update):
     'Check if the bot is alive'
-    bot.sendMessage(chat_id=update.message.chat_id, text="start")
+    bot.sendMessage(chat_id=update.message.chat_id, text="BILI BILI")
