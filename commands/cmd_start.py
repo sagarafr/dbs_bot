42f426c75a9da11a5148fda538c4ea55e99101c7
sagarafr <sagarafr@gmail.com>
2017-05-28 07:27:33 +0200

    chore(cmd): Remove output

--- commands/cmd_start.py
+++ commands/cmd_start.py
@@ -4,5 +4,4 @@ from commands import Command
 @Command()
 def start(bot, update):
     'Check if the bot is alive'
-    print("start")
     bot.sendMessage(chat_id=update.message.chat_id, text="start")
