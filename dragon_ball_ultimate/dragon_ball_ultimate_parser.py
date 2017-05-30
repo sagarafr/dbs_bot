1c7a7253c2ba0349c3a75cb21690b87049c71fbf
sagarafr <sagarafr@gmail.com>
2017-05-30 23:33:19 +0200

    feat(dragon_ball_ultimate): Store correct data in Article class Store title, resume, date and link in Article object

--- dragon_ball_ultimate/dragon_ball_ultimate_parser.py
+++ dragon_ball_ultimate/dragon_ball_ultimate_parser.py
@@ -40,14 +40,17 @@ class Article:
     def link(self, value):
         self._link = value
 
+    def __str__(self):
+        return "Title: " + self._title + "\nDate: " + self._date + "\nResume: " + self._resume + "\nLink: " + self._link
+
 
 class DragonBallUltimate(HTMLParser):
     def __init__(self):
         super().__init__()
         self._current_tag = ""
-        self._data = []
         self._article = []
         self._link = []
+        self._article_state = 0
 
     def handle_comment(self, data):
         pass
@@ -68,28 +71,39 @@ class DragonBallUltimate(HTMLParser):
         pass
 
     def handle_starttag(self, tag, attrs):
-        if tag == "a" and [str(a[0]) for a in attrs] == ["href", "title"]:
-            self._link.append(attrs)
+        if self._current_tag == "article" and [str(a[0]) for a in attrs] == ["href"] and attrs[0][1] not in self._link:
+            self._link.append(attrs[0][1])
         if tag == "article":
             self._current_tag = tag
-            self._data.append(attrs)
 
     def handle_endtag(self, tag):
         if tag == "article":
             self._current_tag = ""
+            self._article_state = 0
 
     def handle_data(self, data):
         if self._current_tag == "article":
             if not self.is_online_space(data):
-                self._data.append(data)
+                if self._article_state == 0:
+                    self._article.append(Article(title=data))
+                elif self._article_state == 1:
+                    self._article[len(self._article) - 1].date = data
+                elif self._article_state == 3:
+                    self._article[len(self._article) - 1].resume = data
+                self._article_state += 1
 
-    @property
-    def data(self):
-        return self._data
+    def merge_article_and_link(self):
+        for cpt_article in range(0, len(self._article)):
+            if cpt_article < len(self._link):
+                self._article[cpt_article].link = self._link[cpt_article]
 
     @property
     def link(self):
         return self._link
+
+    @property
+    def article(self):
+        return self._article
 
     @staticmethod
     def is_online_space(data: str):
