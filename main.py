a8c4260b9753d8c32dfcadaee14cd817210a4280
sagarafr <sagarafr@gmail.com>
2017-06-01 01:28:03 +0200

    feat(main): Integret the DragonBallUltimateNews class

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from configuration.basic_configuration import BasicConfiguration
 from admin.admin import admin
 from panda_stream.panda_stream_parser import PandaStreamParser
+from dragon_ball_ultimate.dragon_ball_ultimate_news import DragonBallUltimateNews
 
 
+# TODO change this. Put all this inside of an object
 config_file = BasicConfiguration('./resources/configuration.ini')
 admin_grp = admin(config_file.admins)
 panda_stream = PandaStreamParser()
+db_ultimate = DragonBallUltimateNews()
 
 
 def main():
